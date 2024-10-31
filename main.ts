b62e2db86d0767733fb66d6891dda8b7eb8071ab
Alexandru Smochina <146827365+AlexSmochina@users.noreply.github.com>
2024-10-31 19:30:21 +0100

    Commit

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    if (input.temperature() > 22) {
+        basic.showString("Calor")
+    } else {
+        basic.showString("Frio")
+    }
 })
+input.onButtonPressed(Button.B, function () {
+    llum = input.lightLevel()
+    if (llum < 50) {
+        music.play(music.stringPlayable("E D G F B A C5 B ", 120), music.PlaybackMode.UntilDone)
+    } else if (llum > 200) {
+        music.play(music.stringPlayable("E B C5 A B G A F ", 120), music.PlaybackMode.UntilDone)
+    }
+})
+let llum = 0
+basic.showIcon(IconNames.Heart)
